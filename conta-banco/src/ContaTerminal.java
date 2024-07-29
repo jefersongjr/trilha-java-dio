56f452e712cb3bbe35bd2758da4583f628f38c27
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-07-29 20:03:31 -0300

    feat :sparkles: criando sistema simples de banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da sua conta:");
+        int conta = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da sua Agencia:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite seu nome completo:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Por favor, digite seu saldo:");
+        float saldo = scanner.nextFloat();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +
+                ", conta "+ conta +" e seu saldo "+ saldo +" já está disponível para saque");
+
+        scanner.close();
     }
 }
