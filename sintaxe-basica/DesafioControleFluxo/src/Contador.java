138da5f73ffbbcee74b9befaef353ba812397ae0
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-08-07 16:42:22 -0300

    feat :sparkles: criando desafio de projeto

--- sintaxe-basica/DesafioControleFluxo/src/Contador.java
+++ sintaxe-basica/DesafioControleFluxo/src/Contador.java
@@ -4,17 +4,33 @@ public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
         System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.??;
+        int parametroUm = terminal.nextInt();
         System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.??;
+        int parametroDois = terminal.nextInt();
 
-        try {
-            //chamando o método contendo a lógica de contagem
-            contar(parametroUm, parametroDois);
+        terminal.close();
 
-        }catch (? exception) {
-            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+        contar(parametroUm, parametroDois);
+
         }
 
-    }
+        static void contar (int parametroUm, int parametroDois) {
+            if (parametroUm > parametroDois) {
+                int numeroInteracoes = parametroUm - parametroDois;
+                int numeroAtual = 1;
+                while (numeroAtual <= numeroInteracoes) {
+                    System.out.println(numeroAtual);
+                    numeroAtual++;
+                }
+            }
+        }
+//        try {
+//            //chamando o método contendo a lógica de contagem
+//            contar(parametroUm, parametroDois);
+//
+//        }catch (Exception exception) {
+//            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+//        }
+//
+//    }
 }
